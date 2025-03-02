1d343706265d0c9f22f6b7665115114046f5b371
yumos <zhengel2022@163.com>
2025-03-02 23:19:41 +0800

    add

---
+++ suite/http.go
@@ -0,0 +1,20 @@
+package suite
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+)
+
+// MockPostResponse Post(gin.Server, "/user/", json)
+func MockPostResponse(server http.Handler, path string, data []byte) (*httptest.ResponseRecorder, error) {
+	req, err := http.NewRequest(http.MethodPost, path, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	resp := httptest.NewRecorder()
+	server.ServeHTTP(resp, req)
+	return resp, err
+}
